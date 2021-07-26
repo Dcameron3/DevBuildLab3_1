34b99875459e24225b22fd197a18c71335a18127
Devin Cameron <devincameron@quickenloans.com>
2021-07-26 15:41:56 -0400

    Initial commit

---
+++ Lab3_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lab3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lab 3.2: Student Information");
+            Console.WriteLine("Hello form lab 3.1");
+        }
+    }
+}
